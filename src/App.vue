1c24195aca89b341833bac95b6295fa265507267
Daniel Amaral <danielamaral652@gmail.com>
2024-05-15 21:33:33 -0300

    primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import Banner from './components/Banner.vue';
+import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+
+
+export default{
+  components: {Banner, ConteudoPrincipal }
+}
+</script>
+
+<template>
+  <Banner />
+  <ConteudoPrincipal />
+</template>
+
+
+
