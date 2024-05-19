40dc8484d105688591b07d2e0403613382766843
Daniel Amaral <danielamaral652@gmail.com>
2024-05-19 10:46:44 -0300

    Correção codigo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <script lang="ts">
 import Banner from './components/Banner.vue';
 import ConteudoPrincipal from './components/ConteudoPrincipal.vue';
+import Rodape from './components/Rodape.vue';
 
-
-export default{
-  components: {Banner, ConteudoPrincipal }
+export default {
+  components: { Banner, ConteudoPrincipal, Rodape }
 }
 </script>
 
 <template>
   <Banner />
   <ConteudoPrincipal />
+  <Rodape />
 </template>
-
-
-
--- src/interfaces/ICategoria.ts
+++ src/interfaces/ICategoria.ts
@@ -1,5 +1,5 @@
 export default interface ICategoria {
-    nome: string;
-    ingredientes: string[];
-    imagem: string;
+  nome: string;
+  ingredientes: string[];
+  imagem: string;
 }
